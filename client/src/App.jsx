045c3a20331022bbd401c1300f64a8f678d96d53
Eray Nurtekin <eray.nurtekin@hotmail.com>
2023-02-13 09:56:38 +0300

    dall-e prod host eklemesi yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { logo } from './assets';
+import { photoCreator } from './assets';
 
 import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between items-center bg-white sm:px-10 px-8 py-3 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          <img src={photoCreator} alt="logo" className="w-28 object-contain" />
         </Link>
-        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white
+        <Link to="/create-post" className='font-inter font-medium bg-[#545455] text-white
         rounded-md px-4 py-2'>
           Create
         </Link>
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,10 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
-
+import photoCreator from './photoCreator.png';
 export {
   download,
   logo,
   preview,
+  photoCreator,
 };
